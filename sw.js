0af74c15b75cdd30a8984077245cf76d14ae80a1
Villager A <100194237+habataku@users.noreply.github.com>
2022-03-26 13:19:17 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,10 @@ importScripts('./uv/uv.sw.js');
 
 const sw = new UVServiceWorker();
 
+sw.on('request', event => {
+
+})
+
 self.addEventListener('fetch', event =>
     event.respondWith(
         sw.fetch(event)
