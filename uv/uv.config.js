7fc11de88c7f01a57d0ffa4f63a6b4e8909b9d7d
Villager A <100194237+habataku@users.noreply.github.com>
2022-03-28 13:23:31 +0900

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/web/',
+    prefix: '',
     bare: 'https://ultra-virid.vercel.app/bare/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
